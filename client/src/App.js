f5d5a6e386def6e64ee5df768bada9c8c1b3e042
Harry Gothold <harrygot@hotmail.com>
2019-07-07 09:29:15 +0100

    created react project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>My App</div>
+      </div>
+    );
+  }
+}
+
+export default App;
