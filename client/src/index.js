cbda818be9bb92b06ae3854230ffbc161bb4d19b
Harry Gothold <harrygot@hotmail.com>
2019-07-07 14:21:38 +0100

    added header and footer and redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "./resources/css/styles.css";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducer from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
