da29ed179f5c9086c3b90e9844cfb4552f4addb3
Harry Gothold <harrygot@hotmail.com>
2019-07-15 22:02:09 +0100

    added filter checkboxes

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import PageTop from "../utils/PageTop";
-
+import { frets } from "../utils/Forms/FixedCategories";
 import { connect } from "react-redux";
 import { getBrands, getWoods } from "../../actions/products_actions";
+import CollapseCheckbox from "../utils/CollapseCheckbox";
 
 class Shop extends Component {
-  state = {};
+  state = {
+    grid: "",
+    limit: 6,
+    skip: 0,
+    filters: {
+      brand: [],
+      frets: [],
+      wood: [],
+      price: []
+    }
+  };
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getWoods());
   }
+
+  handleFilters = (filters, category) => {
+    const newFilters = { ...this.state.filters };
+    newFilters[category] = filters;
+    this.setState({ filters: newFilters });
+  };
+
   render() {
     const products = this.props.products;
     return (
@@ -18,7 +36,24 @@ class Shop extends Component {
         <div className="container">
           <div className="shop_wrapper">
             <div className="left">
-              <h1>Left</h1>
+              <CollapseCheckbox
+                initState={true}
+                title="Brands"
+                list={products.brands}
+                handleFilters={filters => this.handleFilters(filters, "brand")}
+              />
+              <CollapseCheckbox
+                initState={false}
+                title="Frets"
+                list={frets}
+                handleFilters={filters => this.handleFilters(filters, "frets")}
+              />
+              <CollapseCheckbox
+                initState={true}
+                title="Wood"
+                list={products.woods}
+                handleFilters={filters => this.handleFilters(filters, "wood")}
+              />
             </div>
             <div className="right">
               <h1>Right</h1>
